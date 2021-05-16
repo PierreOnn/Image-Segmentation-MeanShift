13a66e5bafb2405e7f565ec40aa4e1f9484bf728
PrinceWallyy <63965775+PrinceWallyy@users.noreply.github.com>
2021-05-16 21:28:18 +0200

    Images added

--- code/main.py
+++ code/main.py
@@ -5,7 +5,6 @@ from algorithm import *
 from plotclusters3D import *
 import matplotlib.pyplot as plt
 import time
-import cv2
 
 
 def main():
@@ -14,24 +13,23 @@ def main():
     # data = data.transpose()
     # labels, peaks = meanshift_opt(data, 2, 4)
     # plotclusters3D(data, labels, peaks)
-    # plt.savefig('../experiments/3dplots/pts3dplot.png')
+    # plt.savefig('../experiments/plots3D/pts3dplot.png')
     # plt.show()
 
     # image = io.imread('../images/181091.jpg')
     # image_2d = np.reshape(image, (-1, 3))
     # labels, peaks = meanshift_opt(image_2d, 30, 2)
     # plotclusters3D(image_2d, labels, peaks)
-    # plt.savefig('../experiments/3dplots/181091_r10_c4.png')
+    # plt.savefig('../experiments/plots3D/181091_r10_c4.png')
     # plt.show()
 
     t0 = time.time()
     image = io.imread('../images/monalisa.jpg')
-    image_blur = cv2.blur(image,(20,20))
-    segmIm, labels, peaks = imSegment(image_blur, 30, 4, '5D')
+    segmIm, labels, peaks = imSegment(image, 10, 8, '3D')
     t1 = time.time()
     print(t1 - t0)
     io.imshow(segmIm)
-    plt.savefig('../experiments/monalisa_blur_segm_r10_c4_3d.png')
+    plt.savefig('../experiments/monalisa_r10_c8_3d.png')
     io.show()
 
 
